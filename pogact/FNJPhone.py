8a6b76dad7ab669a068a73a52e7a5465ffe6fad7
tockun <toshiaki.tanaka.b52@kyoto-u.jp>
2020-07-04 19:19:13 +0900

    FNJPhone: Use FNJbase.
    * pogact/RPAbase/FNJbase.py: New file.
    
    * pogact/FNJPhone.py: Use FNJbase.  And some fixes.
      - Adopt RPAbase framework.

--- pogact/FNJPhone.py
+++ pogact/FNJPhone.py
@@ -1,38 +1,40 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
-import sys
+# import sys
 import datetime
-import time
-import re
-from selenium import webdriver
+from dateutil.relativedelta import relativedelta
+# import time
+# import re
+# from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
-from webdrivermanager import ChromeDriverManager
+# from webdrivermanager import ChromeDriverManager
 import yaml
 import pandas as pd
+import RPAbase.FNJbase
+import logutils.AppDict
+import logutils.mailreporter
 
-
-class FNJPhone():
+class FNJPhone(RPAbase.FNJbase.FNJbase):
     """
+    Get invoice from FNJ IP-Phone.
     """
     def __init__(self):
         """
         Constructor for this class.
         1. Create instance variable.
         """
-        # pass
-        self.driver = None
-        self.driverinfo = []
-        self.wait = None
-        self.config = {}
-        self.pilot_param = {}
-        self.pilot_result = []
-        # form Katalon++
-        self.accept_next_alert = True
+        super().__init__()
+        self.appdict = logutils.AppDict.AppDict
+        self.appdict.setup(
+            r'FNJPhone', __file__,
+            r'0.1', r'$Rev$', r'Alpha'
+        )
+        self.reporter = logutils.mailreporter.MailReporter(r'smtpconf.yaml', self.appdict.name)
 
 
     def prepare(self):
@@ -42,12 +44,14 @@ class FNJPhone():
         1. Update webdriver, if needed.
         1. Prepare work file/folder, and so on.
         """
-        # pass
-        try:
-            with open(r'settings.yaml', 'r') as f:
-                self.config = yaml.safe_load(f)
-        except Exception as e:
-            print(e.args)
+        super().prepare(self.appdict.name)
+        self.today = datetime.datetime.strptime(
+            datetime.datetime.now().strftime("%Y-%m-%d 00:00:00"),
+            "%Y-%m-%d 00:00:00",
+        )
+        self.logger.debug(f"{self.today.strftime('%c')}")
+        self.logger.info(f"@@@Start {self.appdict.name}({self.appdict.version_string()})")
+
         if self.config.get('html_string') is None:
             self.config['html_string'] = '''
 <html>
@@ -79,153 +83,159 @@ class FNJPhone():
 }
 -->
 '''
-        # Driver
-        dm = ChromeDriverManager()
-        self.driverinfo = dm.download_and_install()
-        # prameter
         now = datetime.datetime.now()
-        diff = 62 if now.day <= 15 else 31
-        target_day = now - datetime.timedelta(days=diff) 
+        diff = relativedelta(months=2) if now.day <= 15 else relativedelta(months=1)
+        target_day = now - diff 
+        self.logger.debug(f'  - taget: {target_day.year}/{target_day.month}')
         self.pilot_param['year'] = target_day.year
         self.pilot_param['month'] = target_day.month
-        print(self.pilot_param)
 
-    def setup(self):
+    def pilot_setup(self):
         """
         Execute web browser, and set up for auto-pilot.
         1. Setting up web browser: OPTIONS, EXTENTIONS, ...
         1. Execute web browser.
         1. Fetch some information(s) with web browser.
         """
-        # ブラウザーを起動
         options = Options()
-        # options.add_argument('--headless')
-        self.driver = webdriver.Chrome(self.driverinfo[1], options=options)
-        self.driver.implicitly_wait(10)
-        # self.wait = Wait(self.driver,3)
-   
-    def login(self):
-        """
-        Log-in target web site.
-        """
-        config = self.config
-        driver = self.driver
+        options.add_argument(r'--headless')
+        # イメージボタンが多用されているため、img表示は必要
+        # options.add_argument(r'--blink-settings=imagesEnabled=false')
+        options.add_experimental_option('useAutomationExtension', False)
+        options.add_experimental_option('excludeSwitches', ['enable-automation'])
 
-        driver.get("https://zeny.cyberhome.ne.jp/user/login.aspx")
-        driver.find_element_by_id("LoginBody1_Tb_Account").click()
-        driver.find_element_by_id("LoginBody1_Tb_Account").send_keys(config['id'])
-        driver.find_element_by_id("LoginBody1_Tb_Passsword").click()
-        driver.find_element_by_id("LoginBody1_Tb_Passsword").send_keys(config['pw'])
-        driver.find_element_by_id("LoginBody1_Btn_Login").click()
-        # pass
-    
+        return super().pilot_setup(options)
+
     def pilot(self):
         """
         Pilot automatically, to get your informations.
         """
+        driver, wait = self.pilot_setup()
+        logger = self.logger
         param = self.pilot_param
-        driver = self.driver
 
-        driver.find_element_by_link_text(u"取引内容の確認").click()
-        driver.find_element_by_id("TransactionBody1_Ddl_Year").click()
-        Select(driver.find_element_by_id("TransactionBody1_Ddl_Year")).select_by_visible_text(str(param['year']))
-        driver.find_element_by_id("TransactionBody1_Ddl_Month").click()
-        Select(driver.find_element_by_id("TransactionBody1_Ddl_Month")).select_by_visible_text(str(param['month']))
-        driver.find_element_by_id("TransactionBody1_Ddl_Month").click()
-        driver.find_element_by_id("TransactionBody1_Btn_Search").click()
+        yyyymm = format(param['year'], '04d') + format(param['month'], '02d')
 
-        driver.save_screenshot(f"FNJPhone-{param['year']}{param['month']}.png")
-        table = driver.find_element_by_xpath(r'//*[@id="TransactionBody1_PanelList"]/table[2]/tbody/tr/td/table')
-        # with open(f"FNJPhone-{param['year']}{param['month']}.html", "w") as f:
-        #     f.write(table.page_source)
-        # //*[@id="TransactionBody1_PanelList"]/table[2]/tbody/tr/td/table/tbody/tr[1]
-        # contents = []
-        # for tr in table.find_elements_by_xpath(r'tbody/tr'):
-        #     row = []
-        #     tds = tr.find_elements_by_xpath(r'td')
-        #     # print(tds)
-        #     if tds == []:
-        #         tds = tr.find_elements_by_xpath(r'th')
-        #     for td in tds:
-        #         row.append(td.text)
-        #     contents.append(row)
+        need_report = 0
 
-        # print(contents)
-        # obj = pd.Series(contents)
-        ps = driver.page_source
-        print(type(ps))
-        print(ps[0:30])
-        obj = pd.read_html(ps, match='サービス名')
-        # obj = pd.read_html(unicode(ps, 'cp932').encode('utf-8') , match='サービス名')
-        # obj = pd.read_html(ps.encode('cp932').decode('utf-8'), match='サービス名')
-        with open(f"FNJPhone-tmp.html", 'w', encoding='utf-8') as fp:
-            fp.write(ps)
-        with open(f"FNJPhone-tmp.html", 'r', encoding='utf-8') as f:
-            ps = f.read()
-        obj = pd.read_html(ps, match='サービス名')
-        print(len(obj))
-        print(obj[4])
-        self.pilot_result.append([f"{param['year']}{param['month']}", obj[4].to_csv()])
-        with open(f"FNJPhone-{param['year']}{param['month']}.html", 'w', encoding='utf-8') as f:
-            # idx = 0
-            # for tab in obj:
-            #     f.write(f"=={idx}==\n")
-            #     f.write(f"{obj[idx]}\n\n")
-            #     idx += 1
-            f.write(
-                self.config['html_string'].format(
-                    css_string=self.config['css_string'],
-                    table=obj[4].to_html(classes='mystyle'),
+        logger.debug(f"Read user & service information form yaml.")
+        # ==============================
+        # Get user information
+        users = self.config.get('users',{})
+        users_rakuten = users.get('FNJ',[])
+        # Get service information
+        svcs = self.config.get('services',{})
+        svcs_rkeiba = svcs.get('FNJPhone',[])
+        if len(users_rakuten) * len(svcs_rkeiba) == 0:
+            self.logger.warn(f"No user({len(users_rakuten)}) or service({len(svcs_rkeiba)}) is found.  exit.")
+            return
+
+        # main loop
+        for user in users_rakuten:
+            logger.info(f"処理開始: user={user['name']}")
+            # ==============================
+            if user['name'] not in svcs_rkeiba:
+                logger.info(f"- User {user['name']} does not use this service.  Skip.")
+                continue
+
+            logger.debug(f" 設定内容確認開始")
+            # ==============================
+            # パスワード, pin 以外をデバッグログに出力
+            wk = {k:v for (k,v) in user.items() if k in ('name','id')}
+            logger.debug(f"  params: {wk}")
+            # 実行記録を確認：当日中の実行は１回に限定したい
+            who = user['id']
+            last_dones = self.last_done.get('FNJPhone',{})
+            last_done = last_dones.get(who)
+            if type(last_done) is not datetime.datetime: last_done = datetime.datetime.min
+            logger.debug(f" {who}: today={self.today} vs last={last_done}")
+            if self.today < last_done:
+                logger.info(f" {who}: Already done today.  SKIP.")
+                self.pilot_result.append(
+                    f"{user['name']}: Already done today.  SKIP."
                 )
-            )
-        # print(contents)
+                continue
 
-        # driver.close()
+            need_report += 1
+            try:
+                logger.info(' FNJ(zeny)にログイン')
+                # ==============================
+                if self.pilot_login(user):
+                    logger.debug(' 取引ページに移動')
+                    # ------------------------------
+                    driver.find_element_by_link_text(u"取引内容の確認").click()
 
-    def logout(self):
-        """
-        Log-out from web site.
-        """
-        # pass
-        driver = self.driver
+                    logger.debug(' 年月を指定')
+                    # ------------------------------
+                    driver.find_element_by_id("TransactionBody1_Ddl_Year").click()
+                    Select(driver.find_element_by_id("TransactionBody1_Ddl_Year")).select_by_visible_text(str(param['year']))
+                    driver.find_element_by_id("TransactionBody1_Ddl_Month").click()
+                    Select(driver.find_element_by_id("TransactionBody1_Ddl_Month")).select_by_visible_text(str(param['month']))
+                    driver.find_element_by_id("TransactionBody1_Ddl_Month").click()
+                    driver.find_element_by_id("TransactionBody1_Btn_Search").click()
 
-        driver.find_element_by_xpath(u"(.//*[normalize-space(text()) and normalize-space(.)='田中俊明'])[1]/following::img[1]").click()
+                    logger.debug(' ハードコピーを採取')
+                    # ------------------------------
+                    driver.save_screenshot(f"FNJPhone-{yyyymm}.png")
+                    table = driver.find_element_by_xpath(r'//*[@id="TransactionBody1_PanelList"]/table[2]/tbody/tr/td/table')
 
+                    logger.debug(' 請求項部分をhtmlで切り出して保存')
+                    # ------------------------------
+                    ps = driver.page_source
+                    logger.debug(' -- Webソースをutf-8に揃えて解析する(tmpファイルを使用)')
+                    with open(f"FNJPhone-tmp.html", 'w', encoding='utf-8') as fp:
+                        fp.write(ps)
+                    with open(f"FNJPhone-tmp.html", 'r', encoding='utf-8') as f:
+                        ps = f.read()
+                    obj = pd.read_html(ps, match='サービス名')
+                    logger.debug(f'  numObj: {len(obj)}, obj[4]={obj[4]} ')
+                    self.pilot_result.append([f"{param['year']}{param['month']}", obj[4].to_csv()])
+                    with open(f"FNJPhone-{yyyymm}.html", 'w', encoding='utf-8') as f:
+                        f.write(
+                            self.config['html_string'].format(
+                                css_string=self.config['css_string'],
+                                table=obj[4].to_html(classes='mystyle'),
+                            )
+                        )
+                    logger.debug(' 請求項部分をhtmlで切り出して保存')
+                    # ------------------------------
+                    last_dones[who] = datetime.datetime.now()
+                    self.last_done['FNJPhone'] = last_dones
+                    logger.info(f" {who}: Complete at {last_dones[who].strftime('%c')}.")
 
-    def is_element_present(self, how, what):
-        try: self.driver.find_element(by=how, value=what)
-        except NoSuchElementException: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.driver.switch_to_alert()
-        except NoAlertPresentException: return False
-        return True
-    
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.driver.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally: self.accept_next_alert = True
-    
-    def tearDown(self):
-        self.driver.quit()
-        print(self.pilot_result)
+                else:
+                    logger.error(f'  ログイン失敗！')
+
+            except (TimeoutException, NoSuchElementException) as e:
+                self.pilot_result.append(f"{user['name']}: Ex={type(e)}")
+                logger.error(e.args)
+            except Exception as e:
+                self.pilot_result.append(f"{user['name']}: Ex={type(e)} {e.args}")
+                # self.pilot_result.append(f"{user['name']}: Unexpected error: {sys.exc_info()[0]}")
+                logger.critical(f"Exception: {type(e)} {e.args}")
+
+            finally:
+                logger.info(' ポップアップ終了～ログアウト')
+                # ==============================
+                self.pilot_logout()
+
+            logger.info(f"処理完了: user={user['name']}")
+
+        logger.info(f"全処理を完了")
+        # ==============================
+        self.tearDown()
+        
+        logger.info(f" 処理結果: {self.pilot_result}, need_report: {need_report}")
+        if need_report > 0 and self.pilot_result != []:
+            self.reporter.report(f" 処理結果: {self.pilot_result}")
+            logger.debug(f" 処理結果を　Mailreporter　経由で送信しました")
+
 
 if __name__ == "__main__":
     try:
         App = FNJPhone()
         App.prepare()
-        App.setup()
-        App.login()
         App.pilot()
-        App.logout()
-        App.tearDown()
     except Exception as e:
         print(e.args)
         if App:
