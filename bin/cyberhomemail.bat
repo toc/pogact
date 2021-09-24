356b417559f71cd9ffb465e0af8af176330d7800
T.Tanaka <toshiaki.tanaka.b52@kyoto-u.jp>
2021-09-25 00:52:42 +0900

    バックグラウンド処理のための設定・スクリプト見直し
    変更         bin/cyberhomemail.bat
    　　　　cyberhomemailのマルチユーザ対応に伴い修正
    
    追加         schedule/Cyberhome Mail.xml
    追加         schedule/FNJPhone請求書.xml
    追加         schedule/Mailでポイント(一般).xml
    追加         schedule/Reboot.xml
    追加         schedule/楽天Web検索.xml
    追加         schedule/楽天競馬.xml
    追加         schedule/残高通知.xml
    　　　　現状のスケジュール設定を追加（ウィンドウズ　タスクスケジューラ向け）

--- bin/cyberhomemail.bat
+++ bin/cyberhomemail.bat
@@ -8,5 +8,5 @@ cd ..\pogact
 echo python cyberhomemail.py
 python -O cyberhomemail.py
 
-echo python cyberhomemail2.py
-python -O cyberhomemail2.py
+@REM echo python cyberhomemail2.py
+@REM python -O cyberhomemail2.py
