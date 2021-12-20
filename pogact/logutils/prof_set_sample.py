c2f6bb52f8fe12abd17a992d3014065c5228af94
T.Tanaka <toshiaki.tanaka.b52@kyoto-u.jp>
2021-12-21 00:10:09 +0900

    Rename testprofile.py to prof_set_sample.py.

--- pogact/logutils/prof_set_sample.py
+++ pogact/logutils/prof_set_sample.py
@@ -2,10 +2,11 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import Select
 from selenium import webdriver
 import os
-userdata_dir = r''  
+userdata_dir = r'../profile/'    # path to profile folder
+username = r'testuser'        # identifier for current user.
 os.makedirs(userdata_dir, exist_ok=True)
 options = webdriver.ChromeOptions()
-options.add_argument('--user-data-dir=' + userdata_dir)
+options.add_argument('--user-data-dir=' + userdata_dir + username)
 driver = webdriver.Chrome(r'', options=options)
 # driver.get("https://mail.aol.com/webmail-std/ja-jp/suite")
 driver.get("https://ecnavi.jp/login/")
